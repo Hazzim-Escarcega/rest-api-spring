4f67eebea503c640852ab51270311267b5dab962
Hazzim-Escarcega <said-hazzim.escarcega@atos.net>
2022-05-06 13:52:47 -0600

    MySQL config

--- src/main/java/atos/upgrade/apirest/controllers/ApiController.java
+++ src/main/java/atos/upgrade/apirest/controllers/ApiController.java
@@ -2,14 +2,10 @@ package atos.upgrade.apirest.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-//Habilitar CORS
 @CrossOrigin("*")
-//Indicarle a Spring que este sera nuestro REST controller
 @RestController
-//Indicarle a Spring que este sera el root del controlador
 @RequestMapping("/")
 public class ApiController {
-    //Primer mapping
     @GetMapping("example")
     public ResponseEntity<Object> index(){
         return ResponseEntity.ok("OK");
